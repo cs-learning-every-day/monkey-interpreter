bbbc5a5699308003b92ec289f83f43ef0ba87393
DuckTyping <1394466835@qq.com>
2023-07-01 20:43:58 +0800

    feat: update

---
+++ lexer/lexer.go
@@ -0,0 +1,106 @@
+package lexer
+
+import (
+	"monkey/token"
+)
+
+type Lexer struct {
+	input string
+	// current position in input (points to current char)
+	position int
+	// current reading position in input (after current char)
+	readPosition int
+	// current char under examination
+	ch byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var t token.Token
+	l.skipWhitespace()
+	switch l.ch {
+	case '+':
+		t = newToken(token.PLUS, l.ch)
+	case '=':
+		t = newToken(token.ASSIGN, l.ch)
+	case ',':
+		t = newToken(token.COMMA, l.ch)
+	case ';':
+		t = newToken(token.SESSION, l.ch)
+	case '(':
+		t = newToken(token.LPAREN, l.ch)
+	case ')':
+		t = newToken(token.RPAREN, l.ch)
+	case '{':
+		t = newToken(token.LBRACE, l.ch)
+	case '}':
+		t = newToken(token.RBRACE, l.ch)
+	case 0:
+		t.Literal = ""
+		t.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			t.Literal = l.readIdentifier()
+			t.Type = token.LookupIdent(t.Literal)
+			return t
+		} else if isDigit(l.ch) {
+			t.Type = token.INT
+			t.Literal = l.readNumber()
+			return t
+		} else {
+			t = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+	l.readChar()
+	return t
+
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
